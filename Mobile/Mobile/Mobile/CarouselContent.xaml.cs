99906dcb02b96129c2cbf6b9a55a01f71bf9edec
Ousrhire Abdellah <Ousrhire.Abdellah@gmail.com>
2020-05-09 19:23:06 +0100

    Init

---
+++ Mobile/Mobile/Mobile/CarouselContent.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CarouselContent : ContentView
+    {
+        public CarouselContent()
+        {
+            InitializeComponent();
+        }
+
+        private Monkey model;
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            if (model != null)
+                model.PropertyChanged -= OnModelPropertyChanged;
+
+            model = BindingContext as Monkey;
+
+            if (model != null)
+                model.PropertyChanged += OnModelPropertyChanged;
+        }
+
+        private void OnModelPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Monkey.Id))
+                EntryMoneky.Focus();
+        }
+    }
+}
---
+++ Mobile/Mobile/Mobile/Models/Monkey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Mobile.Models
+{
+    public class Monkey : BindableObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+
+
+        public static List<Monkey> GetMonkeys()
+        {
+            return new List<Monkey>
+            {
+                new Monkey { Id=1, Name = "Rhesus macaque" , Details = "", Location = "", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Rhesus_macaque_%28Macaca_mulatta_mulatta%29%2C_male%2C_Gokarna.jpg/330px-Rhesus_macaque_%28Macaca_mulatta_mulatta%29%2C_male%2C_Gokarna.jpg"},
+                new Monkey {Id=2, Name = "Mandrill" , Details = "", Location = "", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ed/Mandrill_Albert_September_2015_Zoo_Berlin_%282%29.jpg"},
+                new Monkey {Id=3, Name = "Panamanian White-faced Capuchin" , Details = "", Location = "", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Capuchin_Costa_Rica.jpg/1200px-Capuchin_Costa_Rica.jpg"},
+            };
+        }
+    }
+}
